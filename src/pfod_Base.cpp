d5a07f8221638055727e45a3e7632b83dd3bbdaa
drmpf <drmpf@users.noreply.github.com>
2022-07-19 19:59:52 +1000

    V3.55.1
    added license

--- src/pfod_Base.cpp
+++ src/pfod_Base.cpp
@@ -5,7 +5,7 @@ Base class for all pfod_Base_xxxx classes
 The subclasses pfod_Base_xxx must override all the methods below
 */
 /*
- * (c)2014-2017 Forward Computing and Control Pty. Ltd.
+ * (c)2014-2022 Forward Computing and Control Pty. Ltd.
  * NSW Australia, www.forward.com.au
  * This code is not warranted to be fit for any purpose. You may only use it at your own risk.
  * This code may be freely used for both private and commercial use
